32006109956e9f937b2678de983c3326eb728fd6
Erick <erick.qf.13@gmail.com>
2020-03-07 00:08:18 -0300

    create models and viewls

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from polls.models import Question, Choice
 
-# Create your views here.
+
 def index(request):
-    return HttpResponse("oieee")
+    lasted_questions = Question.objects.order_by('pub_date')[:5]
+    output = ', '.join(q.question_text for q in lasted_questions)
+    return HttpResponse(output)
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
